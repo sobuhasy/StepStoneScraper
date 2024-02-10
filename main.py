caf1388bb0b6552028b5ade4131edb52595eb98b
SobuHasy <sobu.hasy@hotmail.com>
2024-02-10 15:52:57 +0100

    It ran the browser but didn't search further

--- main.py
+++ main.py
@@ -20,6 +20,25 @@ async def main():
     # Clicks the search button
     await page.click('button[type="submit"]')
 
+    # Waits for the search results to be available
+    await page.waitForSelector('.job-element')
 
-
+    # Extracts the search results
+    results = await page.evaluate('''
+        Array.from(document.querySelectorAll('.job-element')).map(jobElement => ({
+            title: jobElement.querySelector('.job-element__title').innerText,
+            company: jobElement.querySelector('.job-element__company').innerText,
+            location: jobElement.querySelector('.job-element__location').innerText,
+            date: jobElement.querySelector('.job-element__date').innerText,
+            link: jobElement.querySelector('.job-element__title a).href
+            }))
+        ''')
     
+    # Prints the search results
+    print(results)
+
+    # Closes the browser
+    await browser.close()
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
